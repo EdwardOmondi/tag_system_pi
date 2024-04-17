7f10591fa84fc4bed8f3ff949b1db50efd057a5c
Edward Omondi <ewotieno470@gmail.com>
2024-04-18 01:02:46 +0300

    web socket hooked  up to backend

--- ui/src/app/environment.ts
+++ ui/src/app/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   wsUrl: 'ws://localhost:8765',
   defaultTimeout: 3000,
-  errorTimeout: 5000
-}
+  errorTimeout: 5000,
+  braceletApi:
+    'https://mobileappstarter.com/dashboards/kidzquad/apitest/user/scan_bracelet',
+};
--- ui/src/app/pages/read/read.component.ts
+++ ui/src/app/pages/read/read.component.ts
@@ -3,18 +3,21 @@ import { environment } from '../../environment';
 import { DialogComponent } from './dialog/dialog.component';
 import { Data } from '../../models/data';
 import { NetworkingService } from '../../networking.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-read',
   standalone: true,
   imports: [DialogComponent],
   templateUrl: './read.component.html',
-  styleUrl: './read.component.scss'
+  styleUrl: './read.component.scss',
 })
 export class ReadComponent {
   private networkingService = inject(NetworkingService);
+  private http = inject(HttpClient);
   private ws!: WebSocket;
-  data: Data | null = null
+  data: Data | null = null;
+
   ngOnInit() {
     this.wsInit();
   }
@@ -25,15 +28,22 @@ export class ReadComponent {
       this.networkingService.updateWsState = true;
     };
     this.ws.onmessage = (event) => {
-      this.data = JSON.parse(event.data);
-      setTimeout(() => {
-        this.data = null;
-      }, environment.defaultTimeout);
+      const data = JSON.parse(event.data);
+      if (data.userId !== undefined) {
+        this.http
+          .post<any>(environment.braceletApi, this.data)
+          .subscribe((response) => {
+            this.data = response;
+            setTimeout(() => {
+              this.data = null;
+            }, environment.defaultTimeout);
+          });
+      }
     };
     this.ws.onerror = (event) => {
       this.networkingService.addError('Error connecting to the WebSocket');
       console.error(event);
-    }
+    };
   }
 
   ngOnDestroy() {
