f4f6c38074593a18e1b11d80e3251328d5006733
Edward Omondi <ewotieno470@gmail.com>
2024-04-30 00:39:42 +0300

    Refactor RFID scanning logic in wspi3.py to use localMFRC522 library and remove unused code

--- wspi3.py
+++ wspi3.py
@@ -7,14 +7,10 @@ import time
 import requests
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from localMFRC522 import MFRC522
 import subprocess
 import logging
 
 logging.basicConfig(level=logging.INFO)
-
-# reader = MFRC522()
-
 
 def get_serial_number():
     cpuinfo = subprocess.run(['cat', '/proc/cpuinfo'], capture_output=True, text=True).stdout
@@ -22,28 +18,6 @@ def get_serial_number():
         if line.startswith('Serial'):
             return line.split(':')[1].strip()
         
-# def read(trailer_block, key, block_addrs):
-#     (status, TagType) = reader.Request(reader.PICC_REQIDL)
-#     if status != reader.MI_OK:
-#         return None, None
-#     (status, uid) = reader.Anticoll()
-#     if status != reader.MI_OK:
-#         return None, None
-#     id = uid
-#     reader.SelectTag(uid)
-#     status = reader.Authenticate(
-#         reader.PICC_AUTHENT1A, trailer_block , key, uid)
-#     data = []
-#     text_read = ''
-#     if status == reader.MI_OK:
-#         for block_num in block_addrs:
-#             block = reader.ReadTag(block_num)
-#             if block:
-#                 data += block
-#         if data:
-#             text_read = ''.join(chr(i) for i in data)
-#     reader.StopAuth()
-#     return id, text_read
 
 async def handle_rfid_scan(websocket, path):
     scanner_id = get_serial_number()
@@ -52,7 +26,6 @@ async def handle_rfid_scan(websocket, path):
     reader = SimpleMFRC522()
     last_submissions = {}
     while True:
-        # id, text = read(7, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], [8])
         id, text = reader.read()
         if id is not None:
             timestamp = int(time.time() * 1000)
@@ -64,11 +37,10 @@ async def handle_rfid_scan(websocket, path):
                 last_submissions[id] = timestamp
                 await websocket.send(json.dumps({'Result': -1,'Message': 'Bracelet scanned. Analyzing data...'}))
                 formData = {
-                    'bracelet_id': 2,
-                    'scanner_id': 2,
-                    # 'bracelet_id':id,
-                    # 'scanner_id': scanner_id,
+                    'bracelet_id':id,
+                    'scanner_id': scanner_id,
                 }
+                logging.info('formData: %s', formData)
                 response = requests.post('https://mobileappstarter.com/dashboards/kidzquad/apitest/user/scan_bracelet', data=formData)
                 if response.status_code == 200:
                     logging.info('response: %s', response.json())
