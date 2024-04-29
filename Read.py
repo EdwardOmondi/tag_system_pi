9b52ff4c7315e40d4223f0c1967de78aaf13a648
Edward Omondi <ewotieno470@gmail.com>
2024-04-29 23:22:54 +0300

    Refactor RFID scanning logic to use MFRC522 library and add id initialization in Read.py

--- Read.py
+++ Read.py
@@ -1,18 +1,35 @@
-#!/usr/bin/env python
+from mfrc522 import MFRC522
 
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+reader = MFRC522()
+def read(trailer_block, key, block_addrs):
+    (status, TagType) = reader.Request(reader.PICC_REQIDL)
+    if status != reader.MI_OK:
+        return None, None
+    (status, uid) = reader.Anticoll()
+    if status != reader.MI_OK:
+        return None, None
+    id = uid
+    reader.SelectTag(uid)
+    status = reader.Authenticate(
+        reader.PICC_AUTHENT1A, trailer_block , key, uid)
+    data = []
+    text_read = ''
+    if status == reader.MI_OK:
+        for block_num in block_addrs:
+            block = reader.ReadTag(block_num)
+            if block:
+                data += block
+        if data:
+            text_read = ''.join(chr(i) for i in data)
+    reader.StopAuth()
+    return id, text_read
 
-reader = SimpleMFRC522()
-print("Started")
-print("Use ctrl+c to exit program")
+trailer_block = 11
+key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+block_addrs = [8,9,10]
+id, text = read(trailer_block, key, block_addrs)
+while not id:
+    id, text = read(trailer_block, key, block_addrs)
 
-try:
-    while True:
-        id, text = reader.read()
-        print(id)
-        print(text)
-except KeyboardInterrupt:
-    print("Ended by user")
-finally:
-    GPIO.cleanup()
+print(id)
+print(text)
