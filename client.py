b911df1622a5de328b1fd50c95796c33f9a7cbc6
Edward Omondi <ewotieno470@gmail.com>
2024-05-02 01:16:53 +0300

    Refactor WebSocket URL initialization, add logging statements, and handle connection closed in server.py

--- client.py
+++ client.py
@@ -7,24 +7,25 @@ import logging
 import RPi.GPIO as GPIO
 
 
-async def hello():
-    uri = "ws://localhost:8765"
+async def handler():
+    uri = "ws://0.0.0.0:8765"
     async with websockets.connect(uri) as websocket:
         reader = SimpleMFRC522()
         try:
             while True:
+                logging.debug("\nWaiting for RFID read...\n")
                 id, text = reader.read()
-                logging.debug(f"ID: {id}")
+                logging.debug("\nID: {id}\n")
 
                 await websocket.send(str(id))
-                logging.debug(f">>> {id}")
+                logging.debug("\n>>> {id}\n")
 
                 greeting = await websocket.recv()
-                logging.debug(f"<<< {greeting}")
+                logging.debug("\n<<< {greeting}\n")
         except KeyboardInterrupt:
-            logging.debug("Ended by user")
+            logging.debug("\nEnded by user\n")
         finally:
             GPIO.cleanup()
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(handler())
--- server.py
+++ server.py
@@ -2,18 +2,43 @@
 
 import asyncio
 import websockets
+import requests
+import subprocess
+import logging
+import json
 
-async def hello(websocket):
-    name = await websocket.recv()
-    print(f"<<< {name}")
+connected = set()
 
-    greeting = f"Hello {name}!"
+def get_serial_number():
+    cpuinfo = subprocess.run(['cat', '/proc/cpuinfo'], capture_output=True, text=True).stdout
+    for line in cpuinfo.split('\n'):
+        if line.startswith('Serial'):
+            return line.split(':')[1].strip()
 
-    await websocket.send(greeting)
-    print(f">>> {greeting}")
+async def handler(websocket):
+    # Register.
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            # Broadcast to all connected clients.
+            body={'piId': get_serial_number(), 'braceletId': message,'status': 'INITIAL_SCAN','response': None}
+            logging.info('\nbody: %s\n', body)
+            await asyncio.wait([ws.send(json.dumps(body)) for ws in connected])
+            formData = {
+                    'bracelet_id':id,
+                    'scanner_id': scanner_id,
+            }
+            response = requests.post('https://mobileappstarter.com/dashboards/kidzquad/apitest/user/scan_bracelet', data=formData)
+            body['status'] = 'SCAN_COMPLETE'
+            body['response'] = response.json()
+            logging.info('\nbody: %s\n', body)
+            await asyncio.wait([ws.send(json.dumps(body)) for ws in connected])
+    finally:
+        # Unregister.
+        connected.remove(websocket)
 
 async def main():
-    async with websockets.serve(hello, "localhost", 8765):
+    async with websockets.serve(handler, "", 8765):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
