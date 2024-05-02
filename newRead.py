408ca29cabbeda1a1ff2ddc0edb18f0a6c2fbbfc
Edward Omondi <ewotieno470@gmail.com>
2024-05-02 22:51:31 +0300

    Refactor RFID reading logic and update serial port handling in newRead.py

--- newRead.py
+++ newRead.py
@@ -1,42 +1,17 @@
-# import time
-
-# id = 1
-# print ("Start...")
-# try:
-#     with open('/dev/tty0', 'r') as tty:
-#         while True:
-#             rfid = tty.readline()
-#             time.sleep(0.5)
-#             if rfid:
-#                 rfid = str(rfid)[:10]
-#                 msg = rfid
-#                 print (msg)
-#             time.sleep(0.5)
-# except KeyboardInterrupt:
-#     print("Ended by user")
-
-# import serial
-
-# serialPort = serial.Serial("/dev/tty0", 9600, timeout=0.5)
-
-# try:
-#     while True:
-#         command = serialPort.read_until('\0', size=None)
-#         commandString = command.decode('utf-8')
-
-#         if len(commandString) > 0:
-#             print(commandString)
-
-# except KeyboardInterrupt:
-#     print("Program terminated")
-
 from evdev import InputDevice, categorize, ecodes
 
 # Change /dev/input/eventX to the correct path for your device.
-dev = InputDevice('/dev/input/mouse0')
+dev = InputDevice('/dev/input/event0')
 
 print("Starting to read from RFID reader...")
 
+rfid_id = ""
 for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
-        print(categorize(event))
+        key_event = categorize(event)
+        if key_event.keystate == key_event.key_down:
+            if key_event.keycode == "KEY_ENTER":
+                print("RFID ID: ", rfid_id)
+                rfid_id = ""
+            else:
+                rfid_id += key_event.keycode[-1]
