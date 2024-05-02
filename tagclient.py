e7f7c524d43c207aa3b728f8e7608ca60d3310ed
Edward Omondi <ewotieno470@gmail.com>
2024-05-02 23:05:59 +0300

    Refactor RFID reading logic and update serial port handling in tagclient.py

--- tagclient.py
+++ tagclient.py
@@ -5,15 +5,38 @@ import websockets
 from mfrc522 import SimpleMFRC522
 import logging
 import RPi.GPIO as GPIO
+from evdev import InputDevice, categorize, ecodes
 
 logging.basicConfig(level=logging.INFO)
+
+async def mfrc522Scanner(websocket):
+    reader = SimpleMFRC522()
+    logging.debug("\nWaiting for RFID read...\n")
+    id, text = reader.read()
+    logging.info("\nID: %s\n", id)
+    await websocket.send(str(id))
+
+async def usbScanner(websocket):
+    dev = InputDevice('/dev/input/event0')
+    logging.debug("\nWaiting for RFID read...\n")
+    rfid_id = ""
+    for event in dev.read_loop():
+        if event.type == ecodes.EV_KEY:
+            key_event = categorize(event)
+            if key_event.keystate == key_event.key_down:
+                if key_event.keycode == "KEY_ENTER":
+                    logging.info("\nID: %s\n", rfid_id)
+                    await websocket.send(rfid_id)
+                    rfid_id = ""
+                else:
+                    rfid_id += key_event.keycode[-1]
 
 async def handler():
     uri = "ws://0.0.0.0:8765"
     async for websocket in websockets.connect(uri):
         logger.info("\nConnected to server\n")
         try:
-            await oldScannerReading(websocket)
+            await usbScanner(websocket)
         except websockets.ConnectionClosed:
             logging.debug("\nConnection closed\n")
             continue
@@ -23,13 +46,6 @@ async def handler():
         except websockets.exceptions.ConnectionClosedError:
             logging.debug("\nConnection closed unexpectedly\n")
             continue
-
-async def oldScannerReading(websocket):
-    reader = SimpleMFRC522()
-    logging.debug("\nWaiting for RFID read...\n")
-    id, text = reader.read()
-    logging.info("\nID: %s\n", id)
-    await websocket.send(str(id))
 
 if __name__ == "__main__":
     logger = logging.getLogger('websockets')
