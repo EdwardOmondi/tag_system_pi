ec0d644d1f2ed3c3b3f023fe4997f9b40f0f50f0
Edward Omondi <ewotieno470@gmail.com>
2024-05-04 01:11:01 +0300

    Update home component to show connecting message when WebSocket connection is established

--- ui/src/app/pages/home/home.component.ts
+++ ui/src/app/pages/home/home.component.ts
@@ -19,20 +19,26 @@ export class HomeComponent implements OnInit {
   scannerId = '';
   scannerUrl = new FormControl('');
   showInput = false;
-
+  showConnecting = false;
   ngOnInit() {
     this.networkingService.wsState.subscribe((state: boolean) => {
       this.showInput = !state;
+      this.showConnecting = false;
     });
 
     this.networkingService.scannerId.subscribe((id: string) => {
       this.scannerId = id;
+      this.showConnecting = false;
     });
     this.networkingService.connect();
+    this.networkingService.errors.subscribe((errors: string[]) => {
+      this.showConnecting = false;
+    });
   }
 
   connectScanner() {
     console.log('connectScanner', this.scannerUrl.value);
     this.networkingService.wsUrl = this.scannerUrl.value as string;
+    this.showConnecting = true;
   }
 }
