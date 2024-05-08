7c2593def93c52a55eb1bfd0167dc27e8a67041e
Edward Omondi <ewotieno470@gmail.com>
2024-05-08 07:14:12 +0300

    Update defaultTimeout in environment.ts to 1000 milliseconds

--- ui/src/app/environment.ts
+++ ui/src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   wsUrl: 'ws://localhost:8765',
-  defaultTimeout: 3000,
+  defaultTimeout: 1000,
   errorTimeout: 5000,
   braceletApi:
     'https://mobileappstarter.com/dashboards/kidzquad/apitest/user/scan_bracelet',
