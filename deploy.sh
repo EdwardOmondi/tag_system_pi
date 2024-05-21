e2767d29360ce74ad4451b8c142d9bdfbea6de11
Edward Omondi <ewotieno470@gmail.com>
2024-05-21 22:54:06 +0300

    feat: Refactor deployment script for improved readability and performance
    This commit refactors the deployment script in `deploy.sh` to improve readability and remove unnecessary console output. It also enhances the performance by optimizing the file copying process and removing excess files. The script now follows a clearer sequence of steps, making it easier to understand and maintain.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 cd ~/tag_system_pi/
-echo "'removing old browser files...  '"
+echo "removing old browser files..."
 sudo rm -rf /var/www/html/assets*  /var/www/html/favicon*  /var/www/html/index*  /var/www/html/main*  /var/www/html/styles*  /var/www/html/polyfills*
-echo "'copying new files...  '"
+echo "copying new files..."
 sudo cp browser.zip /var/www/html/
-echo "'moving to new directory...  '"
+echo "moving to new directory..."
 cd /var/www/html/
-echo "'unzipping files...  '"
+echo "unzipping files..."
 sudo unzip browser.zip
-echo "'moving new files...  '"
+echo "moving new files..."
 sudo cp -r browser/* /var/www/html/
-echo "'removing excess files...  '"
+echo "removing excess files..."
 sudo rm -rf browser*
-echo "'restarting web server...  '"
+echo "restarting web server..."
 sudo service nginx restart
-echo "'Done!  '"
+echo "Done!"
 cd ~/tag_system_pi/
